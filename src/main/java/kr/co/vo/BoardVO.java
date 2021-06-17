e6f296edd432c8392243f149496b4b3d8862b850
One dohyeon <31878698+Onedoheyn@users.noreply.github.com>
2021-06-17 18:11:30 +0900

    게시판 작성 기능
    컨트롤러 서비스 dao vo 생성
    각각에 기능이 돌아가도록 클래스 생성
    view jsp만들어서 기본view 생성
    작성시 db에 insert 되는것 확인

---
+++ src/main/java/kr/co/vo/BoardVO.java
@@ -0,0 +1,46 @@
+package kr.co.vo;
+
+import java.util.Date;
+
+public class BoardVO {
+	
+	private int bno; //번호
+	private String title; //제목
+	private String content; //내용
+	private String writer; 
+	private Date regdate;
+	
+	//get,set 생성 
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+	
+
+}
